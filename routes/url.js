6c67984378cd43056d6f96ff765f75d86fd001af
Alex Mikhaylov <alex01236@gmail.com>
2019-10-10 22:13:56 +0300

    FUX THIS STUFF

--- routes/url.js
+++ routes/url.js
@@ -3,20 +3,9 @@ const validUrl = require('valid-url');
 const redis = require('redis');
 const {promisify} = require('util');
 const murmurhash = require('murmurhash');
-const url = require('url');
 //redis://h:pbba1ed4b66de98f4ff7683e7c9441a066206a12fd8f968b265635deed31d729a@ec2-3-229-149-241.compute-1.amazonaws.com:9199
-const urlBase = 'https://infinite-inlet-19708.herokuapp.com/';
-const redisURL = url.parse(process.env.REDISTOGO_URL);
-//console.log(process.env.REDISTOGO_URL);
-const client = redis.createClient(redisURL.port, redisURL.hostname);
-client.auth(redisURL.auth.split(":")[1]);
-
-client.on('error', function (err) {
-    // NOTICE: Enters here
-    console.log("Error " + err);
-})
-
-const setnxAsync = promisify(client.setnx).bind(client);
+const urlBase = 'https://infinite-inlet-19708.herokuapp.com';
+const server = require('../server.js');
 
 const router = express.Router();
 
@@ -26,8 +15,7 @@ router.post('/shorten', async function (req, res) {
     if (validUrl.isUri(longUrl)) {
         try {
             console.log('Looks like an URI');
-            let shortenedUrl = murmurhash.v3(longUrl);
-            await setnxAsync(shortenedUrl, longUrl);
+            let shortenedUrl = await server.dbInsert(longUrl);
             res.json(urlBase + '/' + shortenedUrl);
         } catch(e) {
             res.status(500).json('Server error');
--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const url = require('url');
+const {promisify} = require('util');
+const redis = require('redis');
+const murmurhash = require('murmurhash');
 const app = express();
 
 const port = 8080;
+
+const redisURL = url.parse(process.env.REDISTOGO_URL || 'redis://redistogo:de7cb3d72c0d15c801919d25b28110ad@hammerjaw.redistogo.com:11523/');
+//console.log(process.env.REDISTOGO_URL);
+const client = redis.createClient(redisURL.port, redisURL.hostname);
+client.auth(redisURL.auth.split(":")[1]);
+const setnxAsync = promisify(client.setnx).bind(client);
+async function dbInsert(longUrl) {
+    let shortenedUrl = murmurhash.v3(longUrl);
+    await setnxAsync(shortenedUrl, longUrl);
+    return shortenedUrl;
+}
+
+client.on('error', function (err) {
+    // NOTICE: Enters here
+    console.log("Error " + err);
+})
 
 app.use(express.json({ extended: false }));
 
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
+module.exports.dbInsert = dbInsert;
+
 app.listen(process.env.PORT || port);
+
