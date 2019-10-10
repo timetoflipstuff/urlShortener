07d96727a9ff91022b584b27af64e1b823b6bed8
Alex Mikhaylov <alex01236@gmail.com>
2019-10-10 21:24:10 +0300

    Configuring Redis for Heroku

--- routes/url.js
+++ routes/url.js
@@ -6,8 +6,8 @@ const murmurhash = require('murmurhash');
 const url = require('url');
 //redis://h:pbba1ed4b66de98f4ff7683e7c9441a066206a12fd8f968b265635deed31d729a@ec2-3-229-149-241.compute-1.amazonaws.com:9199
 const urlBase = 'https://infinite-inlet-19708.herokuapp.com/';
-const redisURL = url.parse(process.env.REDISTOGO_URL);
-
+const redisURL = url.parse('redis://redistogo:de7cb3d72c0d15c801919d25b28110ad@hammerjaw.redistogo.com:11523/');
+console.log(process.env.REDISTOGO_URL);
 const client = redis.createClient(redisURL.port, redisURL.hostname);
 client.auth(redisURL.auth.split(":")[1]);
 const setnxAsync = promisify(client.setnx).bind(client);
